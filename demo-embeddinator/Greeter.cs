25b01598a52de33c08310618e57149082d8f1c20
Guillaume Bailleul <laibulle@gmail.com>
2020-01-04 00:35:32 +0100

    chore: add async task

--- demo-embeddinator/Greeter.cs
+++ demo-embeddinator/Greeter.cs
@@ -1,20 +1,30 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 //public delegate int HandlePerson(Person person);
 
 public static class Greeter
 {
     private static Person[] persones = {
         new Person("John", "Doe"),
-        new Person("Johna", "Doe"),
         new Person("Marilyn", "Monroe"),
+        new Person("Abraham", "Lincoln"),
+        new Person("Nelson", "Mandela"),
+        new Person("John F.", "Kennedy"),
+        new Person("Martin Luther", "King"),
+        new Person("Winston", "Churchill"),
+        new Person("Donald", "Trump"),
     };
 
-    public static Task<Person[]> Greet()
+    public static Person[] Greet()
     {
-        return Task.Factory.StartNew(() => {
-            return persones;
+        Task.Factory.StartNew(() => {
+            foreach(var p in persones)
+            {
+                System.Console.WriteLine(p.Greet());
+                Task.Delay(1000).Wait();
+            }
         });
+
+        return persones;
     }
 }
 
